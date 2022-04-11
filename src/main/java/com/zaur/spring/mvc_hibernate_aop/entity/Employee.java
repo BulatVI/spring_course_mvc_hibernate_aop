7ca3a19140e084f02cf5858ea07b0073f82e99ca
Bulat <bulat1987@gmail.com>
2022-04-11 23:21:54 +0500

    add project

---
+++ src/main/java/com/zaur/spring/mvc_hibernate_aop/entity/Employee.java
@@ -0,0 +1,75 @@
+package com.zaur.spring.mvc_hibernate_aop.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", length = 15)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "department", length = 20)
+    private String department;
+
+    @Column(name = "salary")
+    private Integer salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String department, Integer salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
